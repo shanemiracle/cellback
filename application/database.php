cd2314ba7e2677f7967819b42840f5ce7021c6f7
shanemiracle <824116664@qq.com>
2019-04-28 11:14:03 +0800

    Update database.php

--- application/database.php
+++ application/database.php
@@ -13,14 +13,14 @@ return [
     // 数据库类型
     'type'           => 'mysql',
     // 服务器地址
-    'hostname'       => '115.236.177.85',
+    'hostname'       => '127.0.0.1',
     // 数据库名
 //    'database'       => 'wk_cell',
     'database'       => 'wk_cell_yiyuan',
     // 用户名
     'username'       => 'admin',
     // 密码
-    'password'       => '2ws4rf6yh',
+    'password'       => '123456',
     // 端口
     'hostport'       => '',
     // 连接dsn
